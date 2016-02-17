f2dcdf0024d268f80ec30757f60e7d775d6779db
Ze-Q <ze.q.zhang@mail.mcgill.ca>
2016-02-17 01:55:58 -0500

    Added basic UI

--- Postal Rate Calculator/Postal Rate Calculator/ViewController.swift
+++ Postal Rate Calculator/Postal Rate Calculator/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var computeButton: UIButton!
+    @IBOutlet weak var widthTextBox: UITextField!
+    @IBOutlet weak var heightTextBox: UITextField!
+    @IBOutlet weak var depthTextBox: UITextField!
+    @IBOutlet weak var weightTextBox: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // process the input to text fields so that user can only put in valid numbers
+        self.widthTextBox.delegate = self
+        self.heightTextBox.delegate = self
+        self.depthTextBox.delegate = self
+        self.weightTextBox.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func computeButtonIsPressed(sender: AnyObject) {
+        print(widthTextBox.text!)
+        print(heightTextBox.text!)
+        print(depthTextBox.text!)
+        print(weightTextBox.text!)
+        
+        if (heightTextBox.text! == "") {
+            print("height is empty")
+        }
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool { // return NO to not change text
+        
+        switch string {
+        case "0","1","2","3","4","5","6","7","8","9":
+            return true
+        case ".":
+            let array = Array(textField.text!.characters)
+            var decimalCount = 0
+            for character in array {
+                if character == "." {
+                    decimalCount++
+                }
+            }
+            
+            if decimalCount == 1 {
+                return false
+            } else {
+                return true
+            }
+        default:
+            let array = Array(string.characters)
+            if array.count == 0 {
+                return true
+            }
+            return false
+        }
+    }
 }
-
---
+++ Postal Rate Calculator/Postal Rate Calculator/rateComputer.swift
@@ -0,0 +1,15 @@
+//
+//  rateComputer.swift
+//  Postal Rate Calculator
+//
+//  Created by Ze Qian Zhang on 2016-02-17.
+//  Copyright © 2016 zexception. All rights reserved.
+//
+
+import Foundation
+
+class RateComputer {
+    func computePostalRate(width: Double, height: Double, depth: Double, weight: Double) -> Double {
+        return 0.0
+    }
+}
