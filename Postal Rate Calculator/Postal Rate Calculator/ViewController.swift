374096bc31e8aabceb99e554c098571d6f7c864c
Ze-Q <ze.q.zhang@mail.mcgill.ca>
2016-02-16 23:51:27 -0500

    Template generated by XCode

---
+++ Postal Rate Calculator/Postal Rate Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postal Rate Calculator
+//
+//  Created by Ze Qian Zhang on 2016-02-16.
+//  Copyright © 2016 zexception. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
