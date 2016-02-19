cc21bceafb6c3f543df5fb6c1c0c4cd24d1510d6
KaichenWang <kaichen.wang@outlook.com>
2016-02-19 12:19:11 -0500

    completed code step-by-step to pass unit test cases one-by-one

--- Postal Rate Calculator/Postal Rate Calculator/rateComputer.swift
+++ Postal Rate Calculator/Postal Rate Calculator/rateComputer.swift
@@ -11,9 +11,37 @@ import Foundation
 class RateComputer {
     func computePostalRate(width: Double, height: Double, depth: Double, weight: Double)
         -> (cost: Double, errorMessage: String) {
-            if (weight <= 30) {
+        if (weight <= 30) {
+            if (width <= 245 && height <= 156 && depth <= 5){
                 return (1.0, "")
             }
+            else {
+                return (1.8, "")
+            }
+        }
+        else if (weight <= 50) {
+            return (1.2, "")
+        }
+        else if (weight <= 100){
+            return (1.8, "")
+        }
+        else if (weight <= 200){
+            return (2.95, "")
+        }
+        else if (weight <= 300){
+            return (4.1, "")
+        }
+        else if (weight <= 400){
+            return (4.7, "")
+        }
+        else if (weight <= 500){
+            return (5.05, "")
+        }
         return (0.0, "")
     }
 }
+
+
+
+
+
--- Postal Rate Calculator/Postal Rate CalculatorTests/Postal_Rate_CalculatorTests.swift
+++ Postal Rate Calculator/Postal Rate CalculatorTests/Postal_Rate_CalculatorTests.swift
@@ -302,4 +302,5 @@ class Postal_Rate_CalculatorTests: XCTestCase {
         
         XCTAssert(result.errorMessage == "")
     }
+    
 }
